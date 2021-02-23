402e8802270904d2c871776814c61708db2da42d
4anth0ny1 <acatullo4@gmail.com>
2021-02-22 20:03:59 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
@@ -41,9 +42,17 @@ class InteractiveRecord
     end 
 
     def save
-        DB[:conn].execute("INSERT INTO #{table_name_for_insert} #{col_names_for_insert} VALUES (?)", "#{values_for_insert}")
+        DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
       
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
     end 
 
+    def self.find_by_name(name)
+        DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
+    end 
+
+    def self.find_by(x)
+        a = DB[:conn].execute("SELECT * FROM #{self.table_name} VALUES #{x}")
+        binding.pry
+    end 
 end
